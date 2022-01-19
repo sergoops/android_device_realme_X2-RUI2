aee4834e96ac10b4239462ca86fb5cd4d44c462b
xboxfanj <jake@aospa.co>
2022-01-19 17:19:22 +0300

    X2: Use speed tuning for performance critical applications The default ART setting is quicken: run DEX code verification and optimize some DEX instructions to get better interpreter performance.
    This commit switches performance critical applications to speed:
    run DEX code verification and AOT-compile all methods.
    
    PRODUCT_DEXPREOPT_SPEED_APPS (New in Android O)
    List of applications that have been identified as core to the products
    and which are desirable to compile with the speed compiler filter.  For
    example, persistent apps such as SystemUI get a chance to use
    profile-guided compilation only at the next reboot, so it may be better
    for the product to have these apps always AOT-compiled.
    
    [@sergoops] Adapt for Krypton (added condition for lawnchair)

--- device.mk
+++ device.mk
@@ -125,6 +125,16 @@ PRODUCT_COPY_FILES += \
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI
+
+ifeq ($(TARGET_BUILD_LAWNCHAIR),true)
+    PRODUCT_DEXPREOPT_SPEED_APPS += Lawnchair
+else
+    PRODUCT_DEXPREOPT_SPEED_APPS += Launcher3QuickStep
+endif
+
 # Display
 PRODUCT_PACKAGES += \
     disable_configstore \

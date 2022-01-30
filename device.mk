88e7537f4a04bb3e0d8c69a37a3898b19aa1960d
Anton Hansson <hansson@google.com>
2022-01-30 17:14:45 +0300

    X2: Remove PRODUCT_AAPT config This config strips unused drawable resources from the device to save space, but it is not particularly effective in practice. (roughly ~30 MB across all partitions).
    Remove it to make the system image more generic.
    
    Bug: 118676832
    Test: make
    Change-Id: I429dc449866fc0b147cade64c42cf41b00698ffe

--- device.mk
+++ device.mk
@@ -38,10 +38,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt

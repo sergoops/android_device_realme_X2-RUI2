3cc6984bec8ae55e0faa71f57ad4661f0e159eed
SebaUbuntu <barezzisebastiano@gmail.com>
2022-01-30 23:21:42 +0300

    X2: Build atrace 1.0 service hal * As per stock.

--- device.mk
+++ device.mk
@@ -56,6 +56,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     libvolumelistener
